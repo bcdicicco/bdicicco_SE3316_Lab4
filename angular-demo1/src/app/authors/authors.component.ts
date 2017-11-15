7291adda0996f3271c9da9680208019e43ba630b
Brandon DiCicco <bdicicco@uwo.ca>
2017-11-15 07:33:58 +0000

    Finish part 1

---
+++ angular-demo1/src/app/authors/authors.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthorService } from '../author.service';
+@Component({
+  selector: 'app-authors',
+  templateUrl: './authors.component.html',
+  styleUrls: ['./authors.component.css']
+  providers: [AuthorService]
+})
+export class AuthorsComponent implements OnInit {
+  title = "Title for the authors page";
+  authors;
+  
+  constructor(authorService: AuthorService) { 
+    this.authors = authorService.getAuthors();
+  }
+
+  ngOnInit() {
+  }
+
+}
